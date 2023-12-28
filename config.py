99492c5afd70821a15096f7512eef58de2ab9a4a
Klim Trotsenko <klim.trotsenko7@gmail.com>
2023-12-28 21:03:33 +0300

    work without terminal 2

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ EnvContext = Literal['local_emulator', 'bstack', 'local_real']
 
 
 class Config(pydantic_settings.BaseSettings):
-    context: EnvContext = 'local_emulator'
+    context: EnvContext = 'bstack'
     timeout: float = 10.0
 
 config = Config()
